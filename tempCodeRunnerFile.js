9db431cbd7b4e918b54ab68363179fdc76a23367
Mubtasim Ahsan Taha <mubtasimtaha@gmail.com>
2024-05-31 19:44:43 +0600

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,35 @@
+// 1.Create a variable called carName, assign the value Volvo to it.
+
+// let carName = "Volvo" ; 
+
+// // 2.On one single line, declare three variables with the following names and values:
+
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+// /*So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both!*/
+
+// let firstName = "John";    // write variable name
+// let lastName = "Doe"    //write variable values 
+// let age = 35 ;      // write variable name and values both 
+
+
+// /* 3.Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y).*/ 
+
+// x = 10;
+// y = 5;
+// x = 50 // what will be the value of x ? 
+
+
+// //4.Use comments to describe the correct data type of the following variables:
+
+// let length = 16; // data type = "Number" 
+// let lastName = "Johnson"; // data type =  "String"
+
+// const x = {
+//   firstName: "John",  
+//   lastName: "Doe"
+// };     // data type = "object" 
